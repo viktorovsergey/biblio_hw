3484852f5ce92c3e386ab298f43b07cc6267e5a4
batyr-crb-admin <ya_zabiyaka@bk.ru>
2023-06-05 15:00:58 +0300

    initial commit

---
+++ src/test/java/pro/sky/biblio/BiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
